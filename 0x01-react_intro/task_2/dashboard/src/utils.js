9ac663ba223652bdfb585094efde7d87900080f3
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-04-08 10:59:45 +0200

    implemented notifications

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -6,3 +6,6 @@ export function getFooterCopy(isIndex) {
     return isIndex ? "Holberton School" : "Holberton School main dashboard";
 }
   
+export function getLatestNotification() {
+    return "<strong>Urgent requirement</strong> - complete by EOD";
+}
