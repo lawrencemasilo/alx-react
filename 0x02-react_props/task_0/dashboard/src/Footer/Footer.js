06bddaa8e0ac5bb1b8d9be30c4c7624337e927b9
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-04-15 11:13:12 +0200

    Testing components

--- 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
+++ 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import { getFullYear, getFooterCopy } from "../utils/utils";
 import './Footer.css';
 
 function Footer() {
+  return (
     <div className='App-footer'>
-        <p>{getFooterCopy(true)}</p>
-        <p>{getFullYear()}</p>
+      <p>{getFooterCopy(true)}</p>
+      <p>{getFullYear()}</p>
     </div>
+  );
 }
-
 
 export default Footer;
--- 0x02-react_props/task_0/dashboard/src/Header/Header.js
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import logo from './holbertonlogo.jpg';
 import './Header.css';
 
 function Header() {
+  return (
     <div className='App-header'>
-        <img src={logo} alt='logo' className='App-logo' />
-        <h1 className='App-Header-Title'>School dashboard</h1>
+      <img src={logo} alt='logo' className='App-logo' />
+      <h1 className='App-Header-Title'>School dashboard</h1>
     </div>
+  );
 }
 
 export default Header;
