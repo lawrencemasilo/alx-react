2ae21315066d68fab00c4c9530791f4b9a7cc9c1
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-04-22 09:14:10 +0200

    added css

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+const updateCounter = _.debounce(() => {
+  const count = parseInt($('#count').text()) || 0;
+  $('#count').text(`${count + 1} clicks on the button`);
+}, 500);
+
+$(document).ready(() => {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button id="startButton">Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  $('#startButton').click(() => {
+    updateCounter();
+  });
+});
