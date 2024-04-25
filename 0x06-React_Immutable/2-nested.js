3cdcab44982f4b20d631e7118fe7e1b0d6b330c4
Neo <neolawrencemasilo@gmail.com>
2024-04-25 15:23:17 +0000

    Accessing nested elements

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,13 @@
+export default function accessImmutableObject(object, array) {
+    let value = object;
+    for (const key of array) {
+        if (value instanceof Map) {
+            value = value.get(key);
+        } else if (typeof value === 'object' && value !== null) {
+            value = value[key];
+        } else {
+            return undefined;
+        }
+    }
+    return value;
+}
