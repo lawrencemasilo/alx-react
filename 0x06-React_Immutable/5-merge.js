56f686f062395eed163b6a92e7fff214ac4f72e4
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-04-25 19:43:41 +0200

    Merge & concat

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,10 @@
+import { List, Map } from 'immutable';
+
+export function concatElements(page1, page2) {
+  return List([...page1, ...page2]);
+}
+
+export function mergeElements(page1, page2) {
+  const mergedMap = Map(page1).merge(page2);
+  return List(mergedMap.values());
+}
