ed24b3126319dc352ab6a0cdeb482b20765acce0
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-04-26 14:55:04 +0200

    nested

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,15 +1,15 @@
 export default function accessImmutableObject(object, array) {
   let result = object;
-  
   for (const key of array) {
     if (result instanceof Map) {
       result = result.getIn([key]);
+      if (result === undefined) return undefined;
     } else if (typeof result === 'object' && result !== null) {
       result = result[key];
+      if (result === undefined) return undefined;
     } else {
       return undefined;
     }
   }
-  
-  return result;
+  return result instanceof Map ? result : new Map();
 }
