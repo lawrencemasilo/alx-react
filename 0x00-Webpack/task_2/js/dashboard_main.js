79882166695dcb9e85d667641825585fd7dfa179
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-05-21 12:17:46 +0200

    updated dashboard_main.js

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,19 +1,19 @@
 import $ from 'jquery';
-import _ from 'lodash';
+import { debounce } from 'lodash';
 
-const updateCounter = _.debounce(() => {
-  const count = parseInt($('#count').text()) || 0;
-  $('#count').text(`${count + 1} clicks on the button`);
-}, 500);
-
-$(document).ready(() => {
+$(document).ready(function() {
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button id="startButton">Click here to get started</button>');
+  $('body').append('<button id="btn">Click here to get started</button>');
   $('body').append('<p id="count"></p>');
   $('body').append('<p>Copyright - Holberton School</p>');
 
-  $('#startButton').click(() => {
-    updateCounter();
-  });
+  let count = 0;
+
+  function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }
+
+  $('#btn').on('click', debounce(updateCounter, 300));
 });
